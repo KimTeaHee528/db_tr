a5ca33146a402ee7ce1c6b202506b5e253f6ad88
kth <spmm528@gmail.com>
2022-07-28 14:56:00 +0900

    a

--- mysql/sql/update.sql
+++ mysql/sql/update.sql
@@ -1,4 +1,7 @@
 use kthdb1;
 
 -- UPDATE tb_member SET dob = '1995-05-28', address = '광주' WHERE seq = 5;
-UPDATE tb_member SET address = '제주도' WHERE seq = 10;
+UPDATE tb_member SET 
+	address = '제주도'
+	,dob = '1995-05-28'
+WHERE seq = 10;
