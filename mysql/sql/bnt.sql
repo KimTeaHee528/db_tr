da7f0dc797bb3ceeebfc2aeecf18c73c0ba91172
kth <spmm528@gmail.com>
2022-08-05 17:39:57 +0900

    a

--- mysql/sql/bnt.sql
+++ mysql/sql/bnt.sql
@@ -1,39 +1,49 @@
 use kthdb1;
 
 -- 로그인 
-
 select * From tb_member a where 1=1 and id="spm528" and pw = "4dsd64"
 ;
 
--- 상품기준 상품 목록 (로봇채크)
+-- 상품기준 상품 목록 (상품, 이미지, 태그, 브랜드)
 select
 p.seq
 ,p.title
 ,p.price
 ,i.image_url
 ,t.tag_name
+,b.brand_name
 from tb_product p
-inner join tb_image i on i.tb_product_seq = p.seq
+inner join tb_image i on i.tb_product_seq = p.seq and i.image_type = 1
 inner join tb_product_tag pt on pt.tb_product_seq = p.seq
-inner join tb_tag t on t.seq = pt.seq
+inner join tb_tag t on t.seq = pt.tb_tag_seq							and (t.tag_name = "로봇" or t.tag_name = "인기")
+inner join tb_brand_product bp on bp.tb_product_seq = p.seq
+inner join tb_brand b on b.seq = bp.tb_brand_seq 						and (b.brand_name = "Lego" or b.brand_name = "손오공")
 where 1=1
-and i.image_type = 1
-and t.tag_name = "로봇"
+-- and p.seq = 1
+																		-- and p.price <= 35000
 ;
 
--- 태그기준 상품 목록
+-- 상품상세 (상품, 이미지, 태그, 브랜드)
 select
 p.seq
 ,p.title
 ,p.price
 ,i.image_url
 ,t.tag_name
-,pt.*
-from tb_tag t
-inner join tb_product_tag pt on i.tb_product_seq = p.seq
+,b.brand_name
+from tb_product p
+left join tb_image i on i.tb_product_seq = p.seq and i.image_type = 1
 inner join tb_product_tag pt on pt.tb_product_seq = p.seq
-inner join tb_tag t on t.seq = pt.seq
+inner join tb_tag t on t.seq = pt.tb_tag_seq							and (t.tag_name = "로봇" or t.tag_name = "인기")
+inner join tb_brand_product bp on bp.tb_product_seq = p.seq
+inner join tb_brand b on b.seq = bp.tb_brand_seq 						and (b.brand_name = "Lego" or b.brand_name = "손오공")
+where 1=1
+-- and p.seq = 1
+																		-- and p.price <= 35000
 ;
+
+
+
 
 
 -- 코드그룹
