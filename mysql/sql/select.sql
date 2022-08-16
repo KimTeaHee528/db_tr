d93cba6559448bf94019d9d8bf9a4a9497cc47ca
kth <spmm528@gmail.com>
2022-08-16 14:44:57 +0900

    a

--- mysql/sql/select.sql
+++ mysql/sql/select.sql
@@ -1,40 +1,75 @@
 use kthdb1;
 
-CREATE TABLE IF NOT EXISTS `kthdb1`.`member2` (
-  `seq` INT NOT NULL AUTO_INCREMENT,
-  `id` VARCHAR(45) NULL,
-  `pw` VARCHAR(45) NULL,
-  `name` VARCHAR(45) NULL,
-  `grade` TINYINT NULL,
-  `address` VARCHAR(45) NULL,
-  `gender` TINYINT NULL,
-  `dob` DATE NULL,
-  PRIMARY KEY (`seq`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci
+
+-- 조인으로
+select
+p.seq
+,p.title
+,p.price
+,i.image_url
+,t.tag_name
+,b.brand_name
+from tb_product p
+	left join tb_image i on i.tb_product_seq = p.seq 
+		and i.image_type = 1
+	inner join tb_product_tag pt on pt.tb_product_seq = p.seq
+	inner join tb_tag t on t.seq = pt.tb_tag_seq 
+		and (t.tag_name = "로봇" or t.tag_name = "인기")
+	inner join tb_brand_product bp on bp.tb_product_seq = p.seq
+	inner join tb_brand b on b.seq = bp.tb_brand_seq 
+		and (b.brand_name = "Lego" or b.brand_name = "손오공")
+	where 1=1
+		and p.price <= 35000
+    -- asc는 정방향 desc는 역방향
+	-- order by p.price asc
+	order by p.seq asc
 ;
 
-SELECT
-	seq
-	,id
-    ,pw
-    ,name
-    ,grade
-    ,address
-    ,gender
-    ,dob
-FROM member2
--- where 1=1
--- 	and seq = 5
--- 	and id = gsdhm528
--- 	and pw = ssbhfd64sd
--- 	and name = 김태희
--- 	and grade = 1
--- 	and address = '광주'
--- 	and gender = 2
--- 	and dob = 1995-05-28
+
+-- 서브쿼리로
+select
+	p.seq
+	,p.title
+	,p.price
+	,(select image_url 
+		from tb_image i 
+        where 
+			i.tb_product_seq = p.seq  
+			and image_type = 1) 
+	as image_url
+	,(select tag_name 
+		from tb_tag t 
+        where 
+			t.seq = pt.tb_tag_seq 
+			and t.tag_name IS NOT NULL 
+            and (t.tag_name = "로봇" or t.tag_name = "인기"))
+	as tag_name
+	,(select brand_name 
+		from tb_brand b 
+        where 
+			b.seq = bp.tb_brand_seq 
+			and (b.brand_name = "Lego" or b.brand_name = "손오공"))
+	as brand_name
+from tb_product p
+	inner join tb_product_tag pt on pt.tb_product_seq = p.seq
+	inner join tb_brand_product bp on bp.tb_product_seq = p.seq
+	where 1=1
+		and p.price <= 35000
+    -- asc는 정방향 desc는 역방향
+	-- order by p.price asc
+	order by p.seq asc
 ;
+
+
+
+
+
+
+
+select * from tb_image;
+
+
+
 
 -- ===================
 -- select 활용
